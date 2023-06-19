6dc89319a393fbb9b7742ae2f3118033fe42bd6d
StanMungai <stanfordkangethe@outlook.com>
2023-06-19 16:43:52 +0300

    A code along of react state.

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [toggle, setToggle] = useState(false)
+
+  function handleClick() {
+    setToggle( (toggle) => !toggle )
+  }
+
+  const color = toggle ? 'red' : 'white'
+
+  return (
+    <button onClick={handleClick} style={{ background: color}}>
+      { toggle ? 'ON' : 'OFF' }
+    </button>
+  )
 }
 
 export default Toggle;
